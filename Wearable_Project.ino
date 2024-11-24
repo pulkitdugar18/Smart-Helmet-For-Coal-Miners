dc26e45e635e79afad1a01ba6b3db9ce72d5b3ca
pulkitdugar18 <142252090+pulkitdugar18@users.noreply.github.com>
2024-11-24 15:32:39 +0530

    Add files via upload
    Final

--- Wearable_Project.ino
+++ Wearable_Project.ino
@@ -1,55 +1,36 @@
-#include <dht.h>        
-#define outPin 7        
-dht DHT;     
+#include <dht.h>
+#define outPin 7
+dht DHT;
 
 
 int chk;
-float hum; 
-float temp; 
+float hum;
+float temp;
 long duration;
 long distance;
-const int irS = 4;
+const int gasS = A0;
 const int light = 6;
 const int light2 = 5;
 const int buzzer = 11;
 const int trigPin = 8;
 const int echoPin = 9;
-void setup()
-{
+void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);   // Sets the echoPin as an Input
   pinMode(light, OUTPUT);
   pinMode(light2, OUTPUT);
-  pinMode(irS, INPUT);
+  pinMode(gasS, INPUT);
   Serial.begin(9600);
- // dht.begin(); // starts the DHT22 sensor
+  // dht.begin(); // starts the DHT22 sensor
 }
 
 
-void lightSensor()
-{
-  int sensorValue = analogRead(A0);
-  Serial.print("Light Sensor value is: ");
-  Serial.println(sensorValue);
-  if (sensorValue < 150)
-  {
-      Serial.println("*** Darkness detected, turning lights ON ***");
-      digitalWrite(light, HIGH);
-  }
-  else 
-    {
-       digitalWrite(light, LOW);
-    }
-}
-
-void irSensor()
-{
-  int senStatus = digitalRead(irS);
-  if(senStatus == 0)
-  { 
-    Serial.println("*** Detected someone's presence, safety alarm ON ***");
+void gasSensor() {
+  int senStatus = analogRead(gasS);
+  if (senStatus < 400) {
+    Serial.println("*** Warning: Detected poisionous gases ***");
     digitalWrite(light2, HIGH);
     digitalWrite(buzzer, HIGH);
     delay(1000);
@@ -57,66 +38,69 @@ void irSensor()
     digitalWrite(buzzer, LOW);
   }
 }
-void temperature()
-{
-  
+void temperature() {
+
   int readData = DHT.read11(outPin);
 
-	float t = DHT.temperature;        // Read temperature
-	float h = DHT.humidity;           // Read humidity
+  float t = DHT.temperature;  // Read temperature
+  float h = DHT.humidity;     // Read humidity
 
-	Serial.print("Temperature = ");
-	Serial.print(t);
-	Serial.print("°C | ");
-	Serial.print((t*9.0)/5.0+32.0);        // Convert celsius to fahrenheit
-	Serial.print("°F  ");
-	Serial.print("  Humidity = ");
-	Serial.print(h);
-	Serial.println("% ");
-  delay(2000); //2 sec delay
-    if(temp > 30)
-    {
-      Serial.println("*** Temperature is above 30C, turning AC on ***");
-    }
-
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.print("°C | ");
+  Serial.print((t * 9.0) / 5.0 + 32.0);  // Convert celsius to fahrenheit
+  Serial.print("°F  ");
+  Serial.print("  Humidity = ");
+  Serial.print(h);
+  Serial.println("% ");
+  delay(2000);  //2 sec delay
+  if (temp > 30) {
+    Serial.println("*** Warning: Temperature is above 30C ***");
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+    delay(1000);
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+  }
 }
 
-void ultrasonic()
-{
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-duration = pulseIn(echoPin, HIGH);
-distance = duration/74/2;
+void ultrasonic() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 74 / 2;
 
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.print(distance);
-Serial.println(" inches");
-if(distance < 7)
-    {
-      Serial.println("*** Water tank is going to be full, turning motor OFF ***");
-    }
-  else 
-  {
-    Serial.println("*** Water tank is still not full, turning motor ON ***");
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" inches");
+  if (distance < 7) {
+    Serial.println("*** Warning: Tunnel is getting narrower ***");
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+    delay(1000);
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+    delay(1000);
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
   }
 }
 
 
-void loop()
-{
-    temperature();
-    delay(100);
-    ultrasonic();
-    delay(100);
-    lightSensor();
-    delay(100);
-    irSensor();
-    delay(100);
-    
+void loop() {
+  temperature();
+  delay(100);
+  ultrasonic();
+  delay(100);
+  gasSensor();
+  delay(100);
 }
-
-   
